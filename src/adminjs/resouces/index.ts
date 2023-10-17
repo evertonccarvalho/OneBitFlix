b06b089da21926f51343c6115ce965760d1c2bd8
Éverton Carvalho <evertonsnake@gmail.com>
2023-10-17 10:21:27 -0300

    Upload de arquivos no AdminJS disco D

--- src/adminjs/resouces/index.ts
+++ src/adminjs/resouces/index.ts
@@ -2,7 +2,7 @@ import { ResourceWithOptions } from "adminjs";
 import { Category, Course, Episode } from "../../models";
 import { CategoryResouceOptions } from "./category";
 import { courseResourceOptions } from "./course";
-import { episodeResourceOptions } from "./episode";
+import { episodeResourceFeatures, episodeResourceOptions } from "./episode";
 export const adminJsResources: ResourceWithOptions[] = [
   {
     resource: Category,
@@ -15,5 +15,6 @@ export const adminJsResources: ResourceWithOptions[] = [
   {
     resource: Episode,
     options: episodeResourceOptions,
+    features: episodeResourceFeatures,
   },
 ];

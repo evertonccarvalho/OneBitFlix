cc1b38917abd4a7b92a452801f63b0015cdf87b4
Éverton Carvalho <evertonsnake@gmail.com>
2023-10-17 08:01:12 -0300

    Relacionamentos entre Modelos

--- src/adminjs/resouces/category.ts
+++ src/adminjs/resouces/category.ts
@@ -1,6 +1,6 @@
 import { ResourceOptions } from "adminjs";
 export const CategoryResouceOptions: ResourceOptions = {
-  navigation: "Catalogo",
+  navigation: "Catálogo",
   editProperties: ["name", "position"],
   filterProperties: ["name", "position", "createdAt", "updateAt"],
   listProperties: ["id", "name", "position"],
--- src/adminjs/resouces/index.ts
+++ src/adminjs/resouces/index.ts
@@ -1,9 +1,14 @@
 import { ResourceWithOptions } from "adminjs";
-import { Category } from "../../models";
+import { Category, Course } from "../../models";
 import { CategoryResouceOptions } from "./category";
+import { courseResourceOptions } from "./course";
 export const adminJsResources: ResourceWithOptions[] = [
   {
     resource: Category,
     options: CategoryResouceOptions,
+  },
+  {
+    resource: Course,
+    options: courseResourceOptions,
   },
 ];

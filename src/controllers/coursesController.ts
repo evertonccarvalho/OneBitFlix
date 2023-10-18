7598103132794945eef5c1eacbf4cacfc3654b79
Éverton Carvalho <evertonsnake@gmail.com>
2023-10-17 23:17:20 -0300

    Buscando por cursos

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { coursesService } from "../services/coursesService";
+import { getPaginationParams } from "../helpers/getPaginationParams";
 
 export const coursesController = {
   //GET /courses/featured
@@ -18,6 +19,22 @@ export const coursesController = {
     try {
       const newestCourses = await coursesService.getTopTenNewest();
       return res.json(newestCourses);
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message });
+      }
+    }
+  },
+
+  //GET /courses/search?name=
+  search: async (req: Request, res: Response) => {
+    const { name } = req.query;
+    const [page, perPage] = getPaginationParams(req.query);
+    try {
+      if (typeof name !== "string")
+        throw new Error("name param must be of type string");
+      const courses = await coursesService.findByName(name, page, perPage);
+      return res.json(courses);
     } catch (err) {
       if (err instanceof Error) {
         return res.status(400).json({ message: err.message });

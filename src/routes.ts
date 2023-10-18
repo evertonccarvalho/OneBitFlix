1e16675a2b90c608e96d5991f37aa6c4e870fa19
Éverton Carvalho <evertonsnake@gmail.com>
2023-10-17 21:58:16 -0300

    Obtendo informações do curso

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import { catetoriesController } from "./controllers/categoriesController";
+import { coursesController } from "./controllers/coursesController";
 const router = express.Router();
 
 router.get("/categories", catetoriesController.index);
 router.get("/categories/:id", catetoriesController.show);
+
+router.get("/courses/:id", coursesController.show);
 export { router };

2f5fdfc8e9578e41ace10f1408162cebbda66c9f
Éverton Carvalho <evertonsnake@gmail.com>
2023-10-31 13:55:20 -0300

    script sequelize

--- src/database/seeders/20231017164115-create-admin-user.js
+++ src/database/seeders/20231017164115-create-admin-user.js
@@ -1,9 +1,3 @@
-// src/database/seeders/XXXXXXXXXXXXXX-create-admin-user.js
-
-// npx sequelize-cli seed:generate --name create-admin-user
-
-// npx sequelize-cli db:seed --seed src/database/seeders/20231017164115-create-admin-user.js
-
 const bcrypt = require("bcrypt");
 
 ("use strict");
@@ -11,23 +5,32 @@ const bcrypt = require("bcrypt");
 module.exports = {
   async up(queryInterface, Sequelize) {
     const hashedPassword = await bcrypt.hash("123456", 10);
+    const existingAdmin = await queryInterface.sequelize.query("SELECT * FROM users WHERE email = :email", {
+      replacements: { email: "admin@email.com" },
+      type: queryInterface.sequelize.QueryTypes.SELECT,
+    });
 
-    await queryInterface.bulkInsert("users", [
-      {
-        first_name: "Admin",
-        last_name: "User",
-        phone: "555-5555",
-        birth: "1990-01-01",
-        email: "admin@email.com",
-        password: hashedPassword,
-        role: "admin",
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-    ]);
+    if (existingAdmin.length === 0) {
+      await queryInterface.bulkInsert("users", [
+        {
+          first_name: "Admin",
+          last_name: "User",
+          phone: "555-5555",
+          birth: "1990-01-01",
+          email: "admin@email.com",
+          password: hashedPassword,
+          role: "admin",
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ]);
+    } else {
+      console.log("O usuário administrador já existe no banco de dados.");
+    }
   },
 
   async down(queryInterface, Sequelize) {
+    // A operação down pode permanecer a mesma, excluindo o usuário com base no email
     await queryInterface.bulkDelete("users", null, {
       where: { email: "admin@email.com" },
     });

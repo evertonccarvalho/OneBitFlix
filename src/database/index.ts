3c240636550f75d6563d9cd8fd972792f5dbbc39
Éverton Carvalho <evertonsnake@gmail.com>
2024-01-03 10:53:21 -0300

    teste dialect sequelize

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,8 +1,14 @@
-import { Sequelize } from "sequelize";
-import { DATABASE_URL } from "../config/environment";
+import { Sequelize } from 'sequelize';
+import { DATABASE_URL } from '../config/environment';
 
 export const sequelize = new Sequelize(DATABASE_URL, {
-  define: {
-    underscored: true,
-  },
+	define: {
+		underscored: true,
+	},
+	dialectOptions: {
+		ssl: {
+			require: true,
+			rejectUnauthorized: false, // Dependendo da configuração do servidor
+		},
+	},
 });

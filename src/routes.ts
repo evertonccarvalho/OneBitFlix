df002f64ddc830b8b97b8e26168156484a1deec3
Éverton Carvalho <evertonsnake@gmail.com>
2024-01-03 12:01:41 -0300

    api e cors and homecontroller

--- src/routes.ts
+++ src/routes.ts
@@ -1,40 +1,55 @@
-import express from "express";
-import { catetoriesController } from "./controllers/categoriesController";
-import { coursesController } from "./controllers/coursesController";
-import { episodesController } from "./controllers/episodesController";
-import { authController } from "./controllers/authController";
-import { ensureAuth, ensureAuthViaQuery } from "./middlewares/auth";
-import { favoritesController } from "./controllers/favoritesController";
-import { likesController } from "./controllers/likesController";
-import { usersController } from "./controllers/usersControlle";
+import express from 'express';
+import { catetoriesController } from './controllers/categoriesController';
+import { coursesController } from './controllers/coursesController';
+import { episodesController } from './controllers/episodesController';
+import { authController } from './controllers/authController';
+import { ensureAuth, ensureAuthViaQuery } from './middlewares/auth';
+import { favoritesController } from './controllers/favoritesController';
+import { likesController } from './controllers/likesController';
+import { usersController } from './controllers/usersControlle';
+import homeController from './controllers/home.controller';
 const router = express.Router();
 
-router.post("/auth/register", authController.register);
-router.post("/auth/login", authController.login);
+router.get('/', homeController.index);
 
-router.get("/categories", ensureAuth, catetoriesController.index);
-router.get("/categories/:id", ensureAuth, catetoriesController.show);
-router.get("/courses/popular", ensureAuth, coursesController.popular);
-router.get("/courses/featured", ensureAuth, coursesController.featured);
-router.get("/courses/newest", coursesController.newest);
-router.get("/courses/search", ensureAuth, coursesController.search);
-router.get("/courses/:id", ensureAuth, coursesController.show);
+router.post('/auth/register', authController.register);
+router.post('/auth/login', authController.login);
 
-router.get("/episodes/stream", ensureAuthViaQuery, episodesController.stream);
+router.get('/categories', ensureAuth, catetoriesController.index);
+router.get('/categories/:id', ensureAuth, catetoriesController.show);
+router.get('/courses/popular', ensureAuth, coursesController.popular);
+router.get('/courses/featured', ensureAuth, coursesController.featured);
+router.get('/courses/newest', coursesController.newest);
+router.get('/courses/search', ensureAuth, coursesController.search);
+router.get('/courses/:id', ensureAuth, coursesController.show);
 
-router.get("/episodes/:id/watchtime", ensureAuth, episodesController.getWatchTime);
-router.post("/episodes/:id/watchtime", ensureAuth, episodesController.setWatchTime);
+router.get('/episodes/stream', ensureAuthViaQuery, episodesController.stream);
 
-router.post("/favorites", ensureAuth, favoritesController.save);
-router.get("/favorites", ensureAuth, favoritesController.index);
-router.delete("/favorites/:id", ensureAuth, favoritesController.delete);
+router.get(
+	'/episodes/:id/watchtime',
+	ensureAuth,
+	episodesController.getWatchTime
+);
+router.post(
+	'/episodes/:id/watchtime',
+	ensureAuth,
+	episodesController.setWatchTime
+);
 
-router.post("/likes", ensureAuth, likesController.save);
-router.delete("/likes/:id", ensureAuth, likesController.delete);
+router.post('/favorites', ensureAuth, favoritesController.save);
+router.get('/favorites', ensureAuth, favoritesController.index);
+router.delete('/favorites/:id', ensureAuth, favoritesController.delete);
 
-router.get("/users/current", ensureAuth, usersController.show);
-router.put("/users/current", ensureAuth, usersController.update);
-router.put("/users/current/password", ensureAuth, usersController.updatePassword);
-router.get("/users/current/watching", ensureAuth, usersController.watching);
+router.post('/likes', ensureAuth, likesController.save);
+router.delete('/likes/:id', ensureAuth, likesController.delete);
+
+router.get('/users/current', ensureAuth, usersController.show);
+router.put('/users/current', ensureAuth, usersController.update);
+router.put(
+	'/users/current/password',
+	ensureAuth,
+	usersController.updatePassword
+);
+router.get('/users/current/watching', ensureAuth, usersController.watching);
 
 export { router };
--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,29 @@
-import dotenv from "dotenv";
+import cors from 'cors';
+import dotenv from 'dotenv';
 dotenv.config();
 
-import express from "express";
-import cors from "cors";
-import { sequelize } from "./database";
-import { adminJsRouter, adminjs } from "./adminjs";
-import { router } from "./routes";
+import express from 'express';
+import { sequelize } from './database';
+import { adminJsRouter, adminjs } from './adminjs';
+import { router } from './routes';
 
 const app = express();
 
 app.use(cors());
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 app.use(express.json());
 
 app.use(adminjs.options.rootPath, adminJsRouter);
 
-app.use(router);
+app.use('/api/', router);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  sequelize.authenticate().then(() => {
-    console.log("DB Connection Sucessfull");
-  });
-  console.log(`Server started sucessfuly at port ${PORT}`);
+	sequelize.authenticate().then(() => {
+		console.log('DB Connection Sucessfull');
+	});
+	console.log(`Server started sucessfuly at port ${PORT}`);
 });
